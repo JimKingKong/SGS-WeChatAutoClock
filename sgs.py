3a85556c3f239fa256a6af1fb03f4cbb1581a2c6
JimKingKong <45095807+JimKingKong@users.noreply.github.com>
2021-03-25 11:01:35 +0800

    Update sgs.py

--- sgs.py
+++ sgs.py
@@ -2,11 +2,16 @@ import requests
 import os
 import logging
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(message)s',
+    datefmt='%Y-%m-%dT%H:%M:%S')
 
 # 获取cookie
 COOKIE = ''
 if os.environ['SGSCOOKIE']!='':
         COOKIE = os.environ['SGSCOOKIE']
+        LOGGING.info(COOKIE)
         cookie_list = COOKIE.split('#')
 url = 'http://wx.sanguosha.com/api/clock/do'
 ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36 QBCore/4.0.1316.400 QQBrowser/9.0.2524.400 Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2875.116 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat'
